2a5adbd68b7d6e9566f6b672684e6fc4c8dda980
Dim Zog <Dim@asdfg.com>
2016-07-15 13:35:55 +0300

    Newer Commitment check the jars needed (the imports)

--- src/Users.java
+++ src/Users.java
@@ -1,13 +1,10 @@
+//package (default)
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.Charset;
- 
-/**
- * @author Crunchify.com
- * 
- */
  
 public class Users {
  
