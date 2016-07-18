85fb5a827020708b2cb8babc2a5f30fb12cf40e4
Dim Zog <Dim@asdfg.com>
2016-07-18 17:00:04 +0300

    GetNew GUI

---
+++ src/GetUsers.java
@@ -0,0 +1,103 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+public class GetUsers {
+
+	private JFrame frame;
+	private final String USER_AGENT = "Mozilla/5.0";
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GetUsers window = new GetUsers();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GetUsers() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnGetAllUsers = new JButton("Get All Users");
+		btnGetAllUsers.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GetUsers http = new GetUsers();
+
+				System.out.println("Testing 1 - Send Http GET request");
+				try {
+					http.sendGet();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnGetAllUsers.setBounds(10, 11, 111, 23);
+		frame.getContentPane().add(btnGetAllUsers);
+		
+		JTextPane AllUsers = new JTextPane();
+		AllUsers.setBounds(10, 45, 392, 72);
+		frame.getContentPane().add(AllUsers);
+	}
+	private void sendGet() throws Exception {
+
+		String url = "http://core.cityrisks.eu/cityrisks/api/";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//print result
+		System.out.println(response.toString());
+
+	}
+}
