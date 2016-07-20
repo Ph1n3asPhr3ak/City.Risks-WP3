a69699035a294f76382b1ad35bd805e4f23d6169
Dim Zog <Dim@asdfg.com>
2016-07-20 15:47:26 +0300

    Done!!! Stin Class "Users" to Tropopoihsa... Prepei na egkatastathoun omos ta:
    org.json.JSONException;
    org.json.JSONObkect;
    org.json.JSONArray;
    
    prokeimenou na treksei sosta sto output....

--- src/Users.java
+++ src/Users.java
@@ -1,44 +1,60 @@
 //package (default)
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.Charset;
- 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
 public class Users {
- 
-	public static void main(String[] args) {
-		System.out.println("\nOutput: \n" + callURL("http://core.cityrisks.eu/cityrisks/api/"));
-	}
- 
-	public static String callURL(String myURL) {
-		System.out.println("Requeted URL:" + myURL);
-		StringBuilder sb = new StringBuilder();
-		URLConnection urlConn = null;
-		InputStreamReader in = null;
-		try {
-			URL url = new URL(myURL);
-			urlConn = url.openConnection();
-			if (urlConn != null)
-				urlConn.setReadTimeout(60 * 1000);
-			if (urlConn != null && urlConn.getInputStream() != null) {
-				in = new InputStreamReader(urlConn.getInputStream(),
-						Charset.defaultCharset());
-				BufferedReader bufferedReader = new BufferedReader(in);
-				if (bufferedReader != null) {
-					int cp;
-					while ((cp = bufferedReader.read()) != -1) {
-						sb.append((char) cp);
-					}
-					bufferedReader.close();
-				}
-			}
-		in.close();
-		} catch (Exception e) {
-			throw new RuntimeException("Exception while calling URL:"+ myURL, e);
-		} 
- 
-		return sb.toString();
-	}
+
+  private static String readAll(Reader rd) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    int cp;
+    while ((cp = rd.read()) != -1) {
+      sb.append((char) cp);
+    }
+    return sb.toString();
+  }
+
+  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+    InputStream is = new URL(url).openStream();
+    try {
+      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+      String jsonText = readAll(rd);
+      JSONObject json = new JSONObject(jsonText);
+      return json;
+    } finally {
+      is.close();
+    }
+  }
+
+  public static void main(String[] args) throws IOException, JSONException {
+    JSONObject json = readJsonFromUrl("http://core.cityrisks.eu/cityrisks/api/crUsers/");
+    JSONArray arr = json.getJSONObject("_embedded").getJSONArray("crUsers");
+    List<String> list = new ArrayList<String>();
+    System.out.println(arr+"\n");
+    for(int i = 0; i < arr.length(); i++){
+    	list.add(arr.getJSONObject(i).getJSONObject("_links").getJSONObject("self").getString("href"));
+    }
+    System.out.println(list+"\n");
+    for(int i=0;i<list.size();i++){
+    	System.out.println(list.get(i));
+    	JSONObject json2 = readJsonFromUrl(list.get(i));
+    	String firstname = json2.getString("firstname");
+    	String lastname = json2.getString("lastname");
+    	//String username = json2.getString("username");
+    	String email = json2.getString("email");
+    	System.out.println(lastname +" "+ firstname +" ("+ email +") \n"+ json2+"\n\n");
+    } 
+    System.out.println("Done");
+  }
 }
